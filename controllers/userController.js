ce18917b246c15941746796fcc04c12c2d18fab3
Marc-AntoineMarie <mmarie.galaxie@gmail.com>
2025-09-19 16:51:25 +0200

    token + verification and routes security

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
+const jwt = require ("jsonwebtoken");
 
 exports.getAllUsers = async (req, res) => {
     try {
@@ -31,6 +32,29 @@ exports.createUser = async (req, res) => {
         res.status(201).json(newUser);
     } catch (error) {
         res.status(400).json({ error: error.message });
+    }
+}
+
+exports.loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }   
+        
+        const token = jwt.sign(
+            { id: user._id, role: user.role }, 
+            process.env.JWT_SECRET, 
+            { expiresIn: process.env.JWT_EXPIRES_IN || "1h" }); 
+
+        res.status(200).json({ token });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
     }
 }
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const verifyToken = require("../middlewares/verifyToken");
 
-router.get("/", userController.getAllUsers);
-router.get("/:id", userController.getUserById);
+// Routes Publiques
+router.post("/login", userController.loginUser);
 router.post("/", userController.createUser);
-router.patch("/:id", userController.updateUser);
-router.delete("/:id", userController.deleteUser);
+
+// Routes Privées
+router.get("/", verifyToken, userController.getAllUsers);
+router.get("/:id", verifyToken, userController.getUserById);
+router.patch("/:id", verifyToken, userController.updateUser);
+router.delete("/:id", verifyToken, userController.deleteUser);
 
 module.exports = router;
